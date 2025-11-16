99f2735b5d36fee1d307271557fc55c7e1573462
prasanthi <santhi@ram.com>
2025-11-16 14:49:30 +0530

    modules

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "terraform" {
+resource "aws_instance" "this" {
   ami           = var.ami_id
   instance_type = var.instance_type
   vpc_security_group_ids = var.aws_security_group
