73344d6a2c44df19f889f407db1e124fd296f94b
prasanthi <santhi@ram.com>
2025-11-16 16:28:52 +0530

    modules-demo

--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
 variable "ami_id" {
   type = string
+  default = "ami-09c813fb71547fc4f"
   description = "this is the ami used for ec2 creation"
 }
 
@@ -10,6 +11,10 @@ variable "aws_security_group" {
 variable "instance_type" {
     type = string
     description = "this instance type is used for ec2 creation"
+    validation {
+      condition = contains(["t3.micro", "t3.small", "t3.medium"], var.instance_type)
+      error_message = "please select t3.micro or t3.small or t3.medium "
+    }
 }
 
 variable "tags" {
